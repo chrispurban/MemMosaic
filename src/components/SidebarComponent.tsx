484fc0f965720ad868096adddbcdcf81893f4ada
chrispurban <chrispurban@gmail.com>
2023-03-21 00:25:53 -0500

    button for returning to origin saves to pocket again

--- src/components/SidebarComponent.tsx
+++ src/components/SidebarComponent.tsx
@@ -12,6 +12,7 @@ import {
 	NEO_canvasID_atom,
 	NEO_user_selector,
 	NEO_note_atom,
+	NEO_pocketID_atom,
 } from "./RecoilComponent";
 import Login from "./LoginComponent";
 
@@ -21,7 +22,7 @@ export default function Sidebar(){
 	//console.log("sidebar component rendered")
 
 	const [ canvasID, canvasIDΔ ] = useRecoilState(NEO_canvasID_atom)
-	//const [ pocketID, pocketIDΔ ] = useRecoilState(pocketID_atom)
+	const [ pocketID, pocketIDΔ ] = useRecoilState(NEO_pocketID_atom)
 	const [ note, noteΔ ] = useRecoilState(NEO_note_atom(canvasID))
 
 	const [ selectors, data ] = useDeviceSelectors(window.navigator.userAgent)
@@ -58,83 +59,84 @@ export default function Sidebar(){
 		// HANDLE CLICKING OUTSIDE COMPONENT ^^^
 		/////////////////////////////////////////////////////////////////////////////////////
 		
-		
+		const user = useRecoilValue(NEO_user_selector)
+
 		return(<>
 			{
 				__x
 				&& note.color
 				&& <div
-				ref={componentRef}
-				style={{
-					position:'absolute',
-					right:!left?`${expanded?0:10}px`:undefined,
-					left:left?`${expanded?0:10}px`:undefined,
-					top:`${expanded?0:10}px`,
-					...(expanded?{width:'260px', height:`100%`, }:{}),
-					zIndex:'1000',
-					padding:`${expanded?10:0}px`,
-					display:`flex`,
-					flexDirection:`column`,
-					gap:`12px`,
-					backgroundColor:recolor(note.color, {hue:0, lum:(-30), sat:0}),
-					outline:`2px solid ${recolor(note.color, {hue:0, lum:(-40), sat:0})}`,
-				}}
-			>
-				<button
+					ref={componentRef}
 					style={{
-						marginLeft:left?undefined:`auto`,
-						marginRight:!left?undefined:`auto`,
-						width:'40px', height:'40px',
-						display: `flex`,
-						alignItems:`center`, justifyContent:`center`,
-						textAlign:`center`,
-						border:`0px`,
-						backgroundColor:recolor(note.color, {hue:0, lum:(-20), sat:0}),
-						outline:`1px solid ${recolor(note.color, {lum:(expanded?-50:-40),hue:0,sat:0})}`,
-						userSelect:`none`,
-					}}
-					onClick={(e)=>{
-						if(left){ // temporary behavior; once history is added, this will become the first option within it
-							canvasIDΔ(currentID)
-							/*
-							if(canvasID == "N 0"){ // you're on the origin
-								//pocketIDΔ(null)
-							}
-							else{
-								if(!pocketID || pocketID == "N 0"){pocketIDΔ(canvasID)} // nothing in pocket, save current location as the return point
-								canvasIDΔ("N 0") // go to origin
-							}
-							*/
-						}
-						else{
-							expandedΔ((v:boolean)=>!v)
-						}
-						e.stopPropagation()
+						position:'absolute',
+						right:!left?`${expanded?0:10}px`:undefined,
+						left:left?`${expanded?0:10}px`:undefined,
+						top:`${expanded?0:10}px`,
+						...(expanded?{width:'260px', height:`100%`, }:{}),
+						zIndex:'1000',
+						padding:`${expanded?10:0}px`,
+						display:`flex`,
+						flexDirection:`column`,
+						gap:`12px`,
+						backgroundColor:recolor(note.color, {hue:0, lum:(-30), sat:0}),
+						outline:`2px solid ${recolor(note.color, {hue:0, lum:(-40), sat:0})}`,
 					}}
 				>
-					<span style={{
-						paddingTop:`${isWindows?0:2}px`,
-						paddingBottom:`${isWindows?3:0}px`,
-						fontSize:`175%`,
-					}}>
-						{left?"🧿":"👤"}
-					</span>
-				</button>
-				{
-					__x
-					&& expanded
-					&&	<div
+					<button
 						style={{
-							display:`flex`,
-							flexDirection:`column`,
-							gap:`20px`,
-							height:`calc(100% - 60px)`,
+							marginLeft:left?undefined:`auto`,
+							marginRight:!left?undefined:`auto`,
+							width:'40px', height:'40px',
+							display: `flex`,
+							alignItems:`center`, justifyContent:`center`,
+							textAlign:`center`,
+							border:`0px`,
+							backgroundColor:recolor(note.color, {hue:0, lum:(-20), sat:0}),
+							outline:`1px solid ${recolor(note.color, {lum:(expanded?-50:-40),hue:0,sat:0})}`,
+							userSelect:`none`,
+						}}
+						onClick={(e)=>{
+							if(left){ // temporary behavior; once history is added, this will become the first option within it
+								canvasIDΔ(currentID)
+								if(canvasID == user.origin){ // you're on the origin
+									pocketIDΔ("")
+								}
+								else{
+									if(!pocketID || pocketID == user.origin){ // nothing in pocket, save current location as the return point
+										pocketIDΔ(canvasID)
+									}
+									canvasIDΔ(user.origin) // go to origin
+								}
+							}
+							else{
+								expandedΔ((v:boolean)=>!v)
+							}
+							e.stopPropagation()
 						}}
 					>
-						{content}
-					</div>
-				}
-			</div>
+						<span style={{
+							paddingTop:`${isWindows?0:2}px`,
+							paddingBottom:`${isWindows?3:0}px`,
+							fontSize:`175%`,
+						}}>
+							{left?"🧿":"👤"}
+						</span>
+					</button>
+					{
+						__x
+						&& expanded
+						&&	<div
+							style={{
+								display:`flex`,
+								flexDirection:`column`,
+								gap:`20px`,
+								height:`calc(100% - 60px)`,
+							}}
+						>
+							{content}
+						</div>
+					}
+				</div>
 			}
 		</>)
 	}
